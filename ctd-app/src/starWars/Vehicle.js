4656741ed816b883b4a29cb3900aa7b4b954fc5f
aokounev <andre.okounev@gmail.com>
2021-12-15 07:18:24 +0300

    commit change rm local storage

--- ctd-app/src/starWars/Vehicle.js
+++ ctd-app/src/starWars/Vehicle.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
 class Vehicle extends Component {
+
+
   constructor(props) {
     super(props)
 
@@ -11,45 +13,11 @@ class Vehicle extends Component {
 
   componentDidMount() {
     const id = this.props.match.params.vehicleId
-    const savedVehicle = localStorage.getItem(`vehicle-${id}`)
-
-    if (savedVehicle) {
-      this.setState({
-        vehicle: JSON.parse(savedVehicle)
-      })
-      return
-    }
-
     fetch(`https://www.swapi.tech/api/vehicles/${id}/`)
       .then(response => response.json())
       .then(json => {
-        this.setState({ vehicle: json })
-        localStorage.setItem(`vehicle-${id}`, JSON.stringify(json))
+        this.setState({ vehicle: json.result.properties })
       })
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevPropsString = JSON.stringify(prevProps.match.params.vehicleId)
-    const updatedPropsString = JSON.stringify(this.props.match.params.vehicleId)
-
-    if (prevPropsString !== updatedPropsString) {
-      const id = this.props.match.params.vehicleId
-      const savedVehicle = localStorage.getItem(`vehicle-${id}`)
-
-      if (savedVehicle) {
-        this.setState({
-          vehicle: JSON.parse(savedVehicle)
-        })
-        return
-      }
-
-      fetch(`https://www.swapi.tech/api/vehicles/${id}/`)
-        .then(response => response.json())
-        .then(json => {
-          this.setState({ vehicle: json })
-          localStorage.setItem(`vehicle-${id}`, JSON.stringify(json))
-        })
-    }
   }
 
   render() {
