c323a315d8855dc279103eb015b288b26bd60a8e
aokounev <andre.okounev@gmail.com>
2022-03-30 12:24:02 +0300

    Embedded JS in JSX

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,9 +1,17 @@
 import * as React from "react";
-const title = "React";
+import { Label } from "semantic-ui-react";
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello {getTitle("World")}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
